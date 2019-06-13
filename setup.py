2756cf60840ee03fe6260eaba836b71cc1cdbb0e
Pablo Forero <github46@pabloforero.eu>
2019-06-13 13:05:50 +0200

    Add an Envirornment for testing
    I add a conda environment for testing, and expand setup

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+setup(
+    name='electopy',
+    version='0.1dev',
+    packages=['electopy',],
+    license='GNU General Public License v3.0',
+    long_description=open('README.txt').read(),
+)
