bb53db0adda264d1aed6e91f601759e6fc01b923
Pablo Forero <github46@pabloforero.eu>
2019-07-16 11:44:25 +0200

    loading docstrings

--- electopy/loading.py
+++ electopy/loading.py
@@ -1,5 +1,3 @@
-# cSpell: disable
-
 import pandas as pd
 
 import urllib.request
@@ -7,6 +5,17 @@ import zipfile
 
 
 def get_mir_dictionary():
+    """Return a dictionary with the file name for each election year.
+
+    A dictionary that contains the file name of the zip file for each election year
+    in the MIR's web page.
+
+    Returns
+    -------
+    election_mir_dictionary: dict
+        Dictionary with the file name for each year's extension.
+    
+    """
 
     election_mir_dictionary = {
         2016: "PROV_02_201606_1.zip",
@@ -28,6 +37,22 @@ def get_mir_dictionary():
 
 
 def get_file_name(year=2016):
+    """Obtain the name of the file for the election results of a year.
+
+    Get the name of the file for the election of the given year. If the year provided
+    is not in the dictionary of files in the MIR, return the year 2016 file name.
+
+    Parameters
+    ----------
+    year: int
+        Year of the election.
+    
+    Returns
+    -------
+    file_name: str
+        File name.
+    
+    """
 
     mir_dictionary = get_mir_dictionary()
 
@@ -45,6 +70,19 @@ def get_file_name(year=2016):
 
 
 def download_election(year=2016, save_folder="past_elections"):
+    """Download for the Ministry's of Interior web page the election results.
+
+    Download from the MIR's web page the zip file for the election in "year", and
+    saves it in the "save_folder".
+
+    Parameters
+    ----------
+    year: int
+        Year of the election.
+    save_folder: str
+        Location of folder where election file is saved.
+
+    """
 
     MIR_ADDRESS = "http://www.infoelectoral.mir.es/infoelectoral/docxl/"
     file_name = get_file_name(year)
@@ -56,6 +94,22 @@ def download_election(year=2016, save_folder="past_elections"):
 
 
 def file_to_df(file_name):
+    """Transform election file into a dataframe.
+
+    Get the saved file with the election. If it is in a zip file, unzip it and obtain
+    the excel file. Read the excel file into a  dataframe.
+
+    Parameters
+    ----------
+    file_name: str
+        Name of the name of the election file.
+
+    Returns
+    -------
+    election_dataframe: DataFrame
+        Election results with the MIR's format.
+
+    """
 
     if file_name.endswith(".zip"):
 
@@ -75,6 +129,29 @@ def file_to_df(file_name):
 
 
 def load_election(file_name=None, year=2016, save_folder="past_elections"):
+    """Load the election results as a dataframe.
+
+    The results are loaded, either providing directly the name of the file, or the year
+    of the election and the folder it is saved, if it comes from the MIR's webpage.
+
+    If a file_name is provided, it would look directly for the file name. Else, it will
+    look for the year's election in  the save_folder.
+
+    Parameters
+    ----------
+    file_name: str
+        Name of the zip/excel file with the election results.
+    year: int
+        Year of the election results.
+    save_folder: int
+        Location of the folder where the year's election results are stored.
+
+    Returns
+    -------
+    election_dataframe: DataFrame
+        Election results with the MIR's format.
+
+    """
 
     if file_name:
 
@@ -99,10 +176,27 @@ def load_election(file_name=None, year=2016, save_folder="past_elections"):
 
             election_dataframe = file_to_df(file_name)
 
-        return election_dataframe
+    return election_dataframe
 
 
 def get_parties(df):
+    """"Obtain the name of the political parties in the election.
+
+    From the format of the MIR, get the name of all the political parties present in the
+    election. Codify those political parties, and save the names with their code as a
+    series. Political parties are the columns.
+
+    Parameters
+    ----------
+    df: DataFrame
+        Election results with the MIR's format.
+
+    Returns
+    -------
+    parties: Series
+        Political parties in the election.
+
+    """
 
     parties = df.loc[0, (df.loc[1] == "Votos")].str.strip()
 
@@ -113,6 +207,23 @@ def get_parties(df):
 
 
 def get_voting_regions(df):
+    """"Obtain the name of the voting regions in the election.
+
+    From the format of the MIR, get the name of all the voting regions present in the
+    election. Codify those voting regions, and save the names with their code as a
+    series. Voting regions are the rows.
+
+    Parameters
+    ----------
+    df: DataFrame
+        Election results with the MIR's format.
+
+    Returns
+    -------
+    regions: Series
+        Voting regions in the election.
+
+    """
 
     col_name = df.columns[df.loc[1] == "Nombre de Provincia"]
     col_code = df.columns[df.loc[1] == "Código de Provincia"]
@@ -127,6 +238,24 @@ def get_voting_regions(df):
 
 
 def get_votes(df):
+    """Create the dataframe with the votes for the election.
+
+    Properly format the dataframe for the votes in the election. The columns of the
+    dataframe are the political parties, and the rows are the voting regions. All the
+    votes are given by political party per region. Columns and rows are indexed with the
+    codification of regions and parties, instead of their name.
+
+    Parameters
+    ----------
+    df: DataFrame
+        Election results with the MIR's format.
+
+    Returns
+    -------
+    votes: DataFrame
+        Votes per region per party in the election result.
+
+    """
 
     parties = get_parties(df)
     col_code = df.columns[df.loc[1] == "Código de Provincia"]
@@ -140,6 +269,24 @@ def get_votes(df):
 
 
 def get_mps(df):
+    """Create a dataframe with the elected members of parliament (mps) for the election.
+
+    Create a formated dataframe with the elected mps in the election. The columns of the
+    dataframe are the political parties, and the rows are the voting regions. All the
+    mps are given by political party per region. Columns and rows are indexed with the
+    codification of regions and parties, instead of their name.
+
+    Parameters
+    ----------
+    df: DataFrame
+        Election results with the MIR's format.
+
+    Returns
+    -------
+    mps: DataFrame
+        Mps per region per party in the election result.
+
+    """
 
     parties = get_parties(df)
     col_code = df.columns[df.loc[1] == "Código de Provincia"]
@@ -153,6 +300,23 @@ def get_mps(df):
 
 
 def distribution_of_mps(mps):
+    """Get the elected members of parliament (mps) for each region.
+
+    Using the elected mps dataframe with the resulting mps elected per party per region,
+    we obtain the distribution of mps per voting region in play in the elections. The
+    elected mps per region are given as a series.
+
+    Parameters
+    ----------
+    mps: DataFrame
+        Mps per region per party in the election result.
+
+    Returns
+    -------
+    distribution: Series
+        Allocated distribution of electable mps per voting region.
+
+    """
 
     distribution = mps.sum(axis=1).astype(int)
 
@@ -160,6 +324,29 @@ def distribution_of_mps(mps):
 
 
 def clean_mir(df):
+    """Extract the parties, regions, votes and distribution of mps from MIR's format.
+
+    From the MIR's dataframe format, the function obtains the political parties, voting
+    regions, votes per region and party, and distribution of mps following the input
+    format used by electopy.
+
+    Parameters
+    ----------
+    df: DataFrame
+        Election results with the MIR's format.
+
+    Returns
+    -------
+    parties: Series
+        Political parties in the election.
+    regions: Series
+        Voting regions in the election.
+    votes: DataFrame
+        Votes per region per party in the election result.
+    distribution: Series
+        Allocated distribution of electable mps per voting region.
+
+    """
 
     parties = get_parties(df)
     regions = get_voting_regions(df)
@@ -171,6 +358,38 @@ def clean_mir(df):
 
 
 def clean_df(votes, distribution):
+    """Extract the parties, regions and votes from a custom dataframe format.
+
+    From a custom dataframe, the function obtains the political parties, voting regions
+     and votes per region and party following the input format used by electopy.
+
+    The dataframe format assumes that the columns have the parties' names and the row
+    index has the voting regions names. All the values in the dataframe represent the
+    votes received per political party in each voting region.
+
+    The distribution of mps has to be provided separately as a series with the index of
+    the series being the name of the voting region, and the value of each series the
+    number of elected mps allocated to that voting region.
+
+    Parameters
+    ----------
+    votes: DataFrame
+        Votes per region per party from the election result.
+    distribution: Series
+        Allocated distribution of electable mps per voting region.
+
+    Returns
+    -------
+    parties: Series
+        Political parties in the election.
+    regions: Series
+        Voting regions in the election.
+    votes: DataFrame
+        Votes per region per party in the election result.
+    distribution: Series
+        Allocated distribution of electable mps per voting region.
+
+    """
 
     regions = pd.Series(data=range(len(votes.index)), index=votes.index)
     votes.rename(regions, inplace=True)
@@ -184,17 +403,4 @@ def clean_df(votes, distribution):
     return parties, regions, votes, distribution
 
 
-def download_map():
-
-    SAVE_FOLDER = "map"
-    MAP_ADDRESS = "https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/"
-    FILE_NAME = "ne_10m_admin_1_states_provinces.zip"
-
-    url = MAP_ADDRESS + FILE_NAME
-    save_location = SAVE_FOLDER + "/" + FILE_NAME
-
-    urllib.request.urlretrieve(url, save_location)
-
-    zip_ref = zipfile.ZipFile(save_location, "r")
-    zip_ref.extractall(SAVE_FOLDER + "/")
-    zip_ref.close()
+# cSpell: ignore inplace astype iloc rsplit nrows xlsx Diputados Código Votos Nombre
